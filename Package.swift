dcaa055ffb1ac0057555bf0551b59bbb0fe3873d
glasfisch3000 <jakob@danckwerts.net>
2024-02-06 23:13:20 +0100

    updated zenea-swift

--- Package.swift
+++ Package.swift
@@ -6,13 +6,13 @@ import PackageDescription
 let package = Package(
     name: "zenea-swift-cli",
     platforms: [
-       .macOS("13.3")
+        .macOS("13.3")
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.20.1"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.63.0"),
-        .package(url: "https://github.com/glasfisch3000/zenea-swift.git", branch: "main")
+        .package(url: "https://github.com/glasfisch3000/zenea-swift.git", from: "1.0.0-alpha6")
     ],
     targets: [
         .executableTarget(
@@ -21,7 +21,7 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
                 .product(name: "_NIOFileSystem", package: "swift-nio"),
-                .product(name: "zenea", package: "zenea-swift")
+                .product(name: "zenea-swift", package: "zenea-swift")
             ]
         )
     ]
