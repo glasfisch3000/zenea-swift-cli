9765b716c087a812cf03543d44eb08a19b4232f7
glasfisch3000 <jakob@danckwerts.net>
2024-02-06 21:21:26 +0100

    added swift-nio dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
-        .package(url: "https://github.com/glasfisch3000/zenea-swift.git", branch: "main")
+        .package(url: "https://github.com/glasfisch3000/zenea-swift.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.63.0")
     ],
     targets: [
         .executableTarget(
@@ -19,6 +20,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                .product(name: "_NIOFileSystem", package: "swift-nio"),
                 .product(name: "zenea", package: "zenea-swift")
             ]
         )
