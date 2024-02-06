2f81397e996e457d90b5c5a54e20474edab08f12
glasfisch3000 <jakob@danckwerts.net>
2024-02-06 20:22:00 +0100

    added string parsing for block source

--- Sources/zenea-cli/sources/BlockSource.swift
+++ Sources/zenea-cli/sources/BlockSource.swift
@@ -1,3 +1,6 @@
+import ArgumentParser
+import NIOFileSystem
+
 import zenea_http
 
 public enum BlockSource {
@@ -9,10 +12,28 @@ extension BlockSource: Hashable {}
 extension BlockSource: Codable {}
 
 extension BlockSource: CustomStringConvertible {
+    public init?(parsing string: String) {
+        if let http = ZeneaHTTPClient.Server(parsing: string) {
+            self = .http(scheme: http.scheme, domain: http.address, port: http.port)
+        } else {
+            let path = FilePath(string)
+            if path.isEmpty { return nil }
+            if !path.isAbsolute { return nil }
+            
+            self = .file(path: path.string)
+        }
+    }
+    
     public var description: String {
         switch self {
         case .file(let path): path
         case .http(let scheme, let domain, let port): "\(scheme)://\(domain):\(port)"
         }
+    }
+}
+
+extension BlockSource: ExpressibleByArgument {
+    public init?(argument: String) {
+        self.init(parsing: argument)
     }
 }
