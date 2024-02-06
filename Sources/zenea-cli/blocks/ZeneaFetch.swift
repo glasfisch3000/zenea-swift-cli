e8a3756f54c1bedcc914747ed589243e13b1bd83
glasfisch3000 <jakob@danckwerts.net>
2024-02-06 17:51:32 +0100

    renamed zenea-fetch and moved stuff to separate file

--- Sources/zenea-cli/blocks/ZeneaFetch.swift
+++ Sources/zenea-cli/blocks/ZeneaFetch.swift
@@ -55,29 +55,3 @@ extension ZeneaFetch {
         }
     }
 }
-
-extension Block {
-    public enum DataFormat: String, ExpressibleByArgument {
-        case raw
-        case hex
-        case base64
-    }
-    
-    public func decode(as format: Block.DataFormat) -> String? {
-        switch format {
-        case .raw:
-            guard let string = String(data: self.content, encoding: .utf8) else { return nil }
-            return string
-        case .hex:
-            return self.content.toHexString()
-        case .base64:
-            return self.content.base64EncodedString()
-        }
-    }
-}
-
-extension Block.ID: ExpressibleByArgument {
-    public init?(argument: String) {
-        self.init(parsing: argument)
-    }
-}
