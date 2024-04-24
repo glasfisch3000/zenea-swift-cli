647b2856626800787c1ec9e05f4d928586bd99be
glasfisch3000 <jakob@danckwerts.net>
2024-04-24 13:19:56 +0200

    removed default main run action

--- Sources/zenea-cli/ZeneaCLI.swift
+++ Sources/zenea-cli/ZeneaCLI.swift
@@ -3,10 +3,4 @@ import ArgumentParser
 @main
 struct ZeneaCLI: AsyncParsableCommand {
     static var configuration: CommandConfiguration = .init(commandName: "zenea", abstract: "A CLI for the Zenea Project Data Layer.", usage: "", discussion: "", version: "0.0.0", shouldDisplay: true, subcommands: [ZeneaList.self, ZeneaCheck.self, ZeneaFetch.self, ZeneaPut.self, ZeneaSync.self, ZeneaDownload.self, ZeneaUpload.self, ZeneaSources.self], defaultSubcommand: nil, helpNames: nil)
-    
-    func run() async throws {
-        var cmd = ZeneaList()
-        cmd.printSources = true
-        try await cmd.run()
-    }
 }
