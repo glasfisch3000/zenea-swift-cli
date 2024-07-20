c41164944e073c6d17f870fae1f007b5d1ab70f9
glasfisch3000 <jakob@danckwerts.net>
2024-07-20 16:05:49 +0200

    update dependencies

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.20.1"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.64.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.21.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.68.0"),
         .package(url: "https://github.com/glasfisch3000/console-kit.git", from: "4.14.3"),
-        .package(url: "https://github.com/zenea-project/zenea-swift.git", from: "3.0.2"),
-        .package(url: "https://github.com/zenea-project/zenea-swift-files.git", from: "1.0.0"),
-        .package(url: "https://github.com/zenea-project/zenea-swift-http.git", from: "1.0.0"),
+        .package(url: "https://github.com/zenea-project/zenea-swift.git", from: "3.1.0"),
+        .package(url: "https://github.com/zenea-project/zenea-swift-files.git", from: "1.2.0"),
+        .package(url: "https://github.com/zenea-project/zenea-swift-http.git", from: "1.1.0"),
         .package(url: "https://github.com/zenea-project/valya-swift.git", from: "1.0.4"),
     ],
     targets: [
@@ -31,7 +31,7 @@ let package = Package(
                 .product(name: "ConsoleKit", package: "console-kit"),
                 .product(name: "zenea-swift", package: "zenea-swift"),
                 .product(name: "zenea-files", package: "zenea-swift-files"),
-                .product(name: "zenea-swift-http", package: "zenea-swift-http"),
+                .product(name: "zenea-http", package: "zenea-swift-http"),
                 .product(name: "valya-swift", package: "valya-swift"),
             ],
             path: "./Sources/zenea-cli"
